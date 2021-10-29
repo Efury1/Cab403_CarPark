ae76964cf8a602de1cc5a2ac118629441be95947
Chinthaka <chinthakajag@gmail.com>
2021-10-29 21:12:58 +1000

    added the todo list for manager

--- manager.c
+++ manager.c
@@ -1,10 +1,48 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <time.h>
-#include <pthread.h>
-#include <semaphore.h>
-#include "airportsim.h"
+//calling in the .h file 
+#include "carparkSim.h"
+
+//calls in the function to make sure the cars are allowed to enter (bool)
+//if true let plate continue if false display "car is banned"
+
+//calls in the license plates for lpr function for each entrance (needs to be interchangeable)
+//stays for 2ms before lpr lets it in
+
+// call in the function to time the boom gates
+//takes 10ms to open , stays open for 10ms and takes 10ms to close 
+
+    //calls in the function to check which levels are free to add cars to park
+    //needs to have a count at all times
+
+    //calls in the function to display if the carpark is full
+    //call in all the levels and check of it is not equal to max per lvl (10/10 or 9/10)
+
+//calls in the function to rand the time car stays at park 
+//start the time as soon as the car enters (maybe add 10ms to exit)  
+
+    //call in the function to multiply the time car stayed by the cost per min
+    //mutiply rand time by dollars
+        
+//call in the function to write the cost of each car parking price to a txt file
+//make new txt if no current and add new lines (REMEMBER TO CLOSE)
+
+//call in the function to time the boom gate opening at the exit
+//make sure it takes 30ms like at open
+
+    //call in the function to display values depending on the returns of functions
+    //use aquas 
+
+
+
+
+
+
+
+
+
+//call in the function that gives the firalarm a rand temp every 5 seconds 
+
+
+
 
 
 int main(int argc, char* argv[]) {
