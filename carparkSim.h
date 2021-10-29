2e59247b3f84c8ebf1b13d2384c3dfece099bfda
Chinthaka <chinthakajag@gmail.com>
2021-10-29 21:25:44 +1000

    added new defines

--- carparkSim.h
+++ carparkSim.h
@@ -1,20 +1,38 @@
-#include <unistd.h>
-#include <time.h>
+
+/*
+*CAB403 Car Park Sim
+*
+*
+*
+*/
+
+//Calling on all libs
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include <pthread.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h> 
+#include <time.h>
 #include <semaphore.h>
 
-#define NUM_OF_LEVEL 5
-#define PARKS 20
-#define BOOMGATE_OPEN  10000
+//defines to make the project flexible
+#define ENTRANCES 5 /*Number of entrances for the carpark*/
+#define EXITS 5  /*Number of exits for the carpark*/
+#define NUM_OF_LEVEL 5 /*Amount of levels in carpark*/
+#define PARKS 20 /*Max amount of cars that can park in each floor*/
+
+#define BOOMGATE_OPEN  10000 /*Microseconds */
 #define BOOMGATE_CLOSE 10000 /*Microseconds */
+
 #define CAR_FINDING_PARK 10000 /*Car takes 10ms to find park */
-
-#define MIN_TIME 100 /*Minimum time car will park */
+#define MIN_TIME 100 /*Minimum time car will park*/
 #define MAX_TIME 10000 /*Max time car will park */
-#define FILE "plates.txt"
 
+#define FILE "plates.txt"
 
 /*Prototypes */
 typedef struct car {
