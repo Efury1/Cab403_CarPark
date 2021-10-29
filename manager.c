090ad3aa4b98e1f21ae1703db0b01fea0c6eac11
Eliza Fury <39455919+Efury1@users.noreply.github.com>
2021-10-29 20:29:03 +1000

    Create manager.c

---
+++ manager.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include "airportsim.h"
+
+
+int main(int argc, char* argv[]) {
+	//Starting state for parking space
+	
+	//Error check terminal incase individuals hasn't call the right thing
+	//Print Enterance header 
+	
+	//Create a semaphore
+	//Error if you can't create semaphore
+	
+	//get inputs from plate and randomePlate
+	
+	//Make pthreads
+	
+	//Print display
+    
+}
+
+/*
+ * Wait for another LPR thread to release the sempare, so plate can be called in
+ * if carsParked < carCount then search for level
+ * carCount++ if it's allowed in
+ * else if cars park is >= 100 (100 being the limit of cars)
+ * 
+ * 
+ *  */
+
+
+
