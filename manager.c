9abd4929f3659d9ec27c4742d4cf8cfeebe14dde
Eliza <efury1@github.com>
2021-10-30 12:23:52 +1000

    Deleted the includes because when when I pull request it will get put in again

--- manager.c
+++ manager.c
@@ -6,17 +6,6 @@
 *
 */
 
-//Calling on all libs
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <pthread.h>
-#include <sys/mman.h>
-#include <sys/types.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <math.h>
-#include <stdbool.h>
 
 //calling in the .h file 
 #include "carparkSim.h"
